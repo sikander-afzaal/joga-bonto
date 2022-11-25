2224de18af13af64ed9301e0038f3195fed2ec64
sikander-afzaal <sikander.aaa@gmail.com>
2022-11-25 19:47:31 +0500

    first page done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./Layout/Header/Header";
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route element={<Home />} path="/" />
+      </Routes>
     </div>
   );
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,27 @@
 import "./styles/Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="container home-page">
+      <img src="/assets/hero-circle.png" className="home-bg" />
+      <div className="home-div">
+        <div className="left-home">
+          <img src="/assets/logo.png" alt="" />
+          <h2>
+            Excited to play the most <br /> <span>thrilling</span> <br />{" "}
+            football game ever? If yes then <br /> hit the <span>PLAY</span>{" "}
+            button at the top.
+          </h2>
+          <div className="btn-div">
+            <button className="cta-btn-inverse bit">PVE</button>
+            <button className="cta-btn-inverse bit">PVP</button>
+            <button className="cta-btn bit">Tournament</button>
+          </div>
+        </div>
+        <img src="/assets/footballer.png" alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
