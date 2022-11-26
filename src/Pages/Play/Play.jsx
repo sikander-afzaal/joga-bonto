4e75d0d851b988fdb4de4921b6393a5581e9d6ae
sikander-afzaal <sikander.aaa@gmail.com>
2022-11-26 12:36:51 +0500

    last page remaining

---
+++ src/Pages/Play/Play.jsx
@@ -0,0 +1,11 @@
+import "./styles/Play.css";
+
+const Play = () => {
+  return (
+    <div className="container play-page">
+      <div className="play-div"></div>
+    </div>
+  );
+};
+
+export default Play;
